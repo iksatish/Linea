82e040eccdab74519721fb5019c25c6fb3fd1e72
Srikanth <gsteja22@gmail.com>
2016-10-06 14:11:33 -0400

    Setup all functionality

---
+++ DSIGLinea/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  DSIGLinea
+//
+//  Created by Satish Kumar R Kancherla on 10/6/16.
+//  Copyright © 2016 DSIG. All rights reserved.
+//
+
+import Foundation
---
+++ DSIGLinea/ServiceProvider.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceProvider.swift
+//  DSIGLinea
+//
+//  Created by Satish Kumar R Kancherla on 10/6/16.
+//  Copyright © 2016 DSIG. All rights reserved.
+//
+
+import Foundation
